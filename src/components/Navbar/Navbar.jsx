870c9b09af11e817b90adaba458b244614b563de
Guilherme webber <guiz1n.webber@gmail.com>
2025-01-08 10:52:45 -0300

    add menu mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 import { FaDownload } from "react-icons/fa";
+
 import "./Navbar.css";
 function Navbar() {
+  const [isChecked, setIsChecked] = useState(false); // Estado para controlar o checkbox
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked); // Atualiza o estado baseado no checkbox
+  };
+
   return (
-    <div className="containerNavbar">
+    <div className={`containerNavbar containerMobile ${isChecked}`}>
       <div className="divLogo">
         <Link className="logo" to="/">
           <p>VALORANT</p>
@@ -52,9 +59,57 @@ function Navbar() {
         <button className="btnDownload">
           {" "}
           <a href="#">
-          <FaDownload className="iconDownload" />
-            Download</a>
+            <FaDownload className="iconDownload" />
+            Download
+          </a>
         </button>
+      </div>
+
+      <div className="divHamburguer">
+        <input
+          type="checkbox"
+          className="inputHamburguer"
+          checked={isChecked} // Aplica o estado ao checkbox
+          onChange={handleCheckboxChange} // Define o evento para mudança
+        />
+        <span className="menuHamburguer"></span>
+      </div>
+
+      <div className="mobOpen">
+        <div className="nav">
+          <ul className="navList">
+            <li>
+              <Link className="linksNav" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="linksNav" to="/agentes">
+                Agentes
+              </Link>
+            </li>
+            <li>
+              <Link className="linksNav" to="/armas">
+                Armas
+              </Link>
+            </li>
+            <li>
+              <Link className="linksNav" to="/mapas">
+                Mapas
+              </Link>
+            </li>
+            <li>
+              <Link className="linksNav" to="/ranks">
+                Ranks
+              </Link>
+            </li>
+            <li>
+              <Link className="linksNav" to="/sobre">
+                Sobre
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
